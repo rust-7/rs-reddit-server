fad19af5037f8cae114a15a9b881f70455edab3a
rust7 <rust.91@icloud.com>
2020-11-03 20:36:00 +0100

    Mock Data

--- dist/index.js
+++ dist/index.js
@@ -13,20 +13,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-const apollo_server_express_1 = require("apollo-server-express");
-const connect_redis_1 = __importDefault(require("connect-redis"));
-const cors_1 = __importDefault(require("cors"));
-const express_1 = __importDefault(require("express"));
-const express_session_1 = __importDefault(require("express-session"));
-const ioredis_1 = __importDefault(require("ioredis"));
-const type_graphql_1 = require("type-graphql");
 const constants_1 = require("./constants");
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
 const hello_1 = require("./resolvers/hello");
 const post_1 = require("./resolvers/post");
 const user_1 = require("./resolvers/user");
+const ioredis_1 = __importDefault(require("ioredis"));
+const express_session_1 = __importDefault(require("express-session"));
+const connect_redis_1 = __importDefault(require("connect-redis"));
+const cors_1 = __importDefault(require("cors"));
 const typeorm_1 = require("typeorm");
 const Post_1 = require("./entities/Post");
 const User_1 = require("./entities/User");
+const path_1 = __importDefault(require("path"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const conn = yield typeorm_1.createConnection({
         type: "postgres",
@@ -35,42 +36,46 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         password: "postgres",
         logging: true,
         synchronize: true,
+        migrations: [path_1.default.join(__dirname, "./migrations/*")],
         entities: [Post_1.Post, User_1.User],
     });
-    yield Post_1.Post.delete({});
+    yield conn.runMigrations();
     const app = express_1.default();
     const RedisStore = connect_redis_1.default(express_session_1.default);
     const redis = new ioredis_1.default();
     app.use(cors_1.default({
         origin: "http://localhost:3000",
-        credentials: true
+        credentials: true,
     }));
     app.use(express_session_1.default({
         name: constants_1.COOKIE_NAME,
         store: new RedisStore({
             client: redis,
-            disableTouch: true
+            disableTouch: true,
         }),
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
             httpOnly: true,
-            sameSite: 'lax',
+            sameSite: "lax",
             secure: constants_1.__prod__,
         },
         saveUninitialized: false,
-        secret: 'hdyrbunshtpldecgfdhuydra',
+        secret: "qowiueojwojfalksdjoqiwueo",
         resave: false,
     }));
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: yield type_graphql_1.buildSchema({
             resolvers: [hello_1.HelloResolver, post_1.PostResolver, user_1.UserResolver],
-            validate: false
+            validate: false,
         }),
-        context: ({ req, res }) => ({ req, res, redis })
+        context: ({ req, res }) => ({ req, res, redis }),
     });
-    apolloServer.applyMiddleware({ app, cors: false });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
     app.listen(4000, () => {
-        console.log('🟢 Server: http://localhost:4000');
+        console.log("server started on localhost:4000");
     });
 });
 main().catch((err) => {
--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,82 @@
 import "reflect-metadata";
-import { ApolloServer } from 'apollo-server-express';
-import connectRedis from 'connect-redis';
-import cors from 'cors';
+import { __prod__, COOKIE_NAME } from "./constants";
 import express from "express";
-import session from 'express-session';
-import Redis from 'ioredis';
+import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { COOKIE_NAME, __prod__ } from "./constants";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import { createConnection } from 'typeorm'
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import { createConnection } from "typeorm";
 import { Post } from "./entities/Post";
 import { User } from "./entities/User";
+import path from "path";
 
+// rerun
 const main = async () => {
-	
-	// Establishing TypeORM Connection
-	const conn = await createConnection({
-		type: "postgres",
-		database: "rustreddit",
-		username: "postgres",
-		password: "postgres",
-		logging: true,
-		synchronize: true,
-		entities: [Post, User],
-	});
+  const conn = await createConnection({
+    type: "postgres",
+    database: "rustreddit",
+    username: "postgres",
+    password: "postgres",
+    logging: true,
+    synchronize: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [Post, User],
+  });
+  
+  await conn.runMigrations();
 
-	await Post.delete({});
+  const app = express();
 
-	const app = express();
-	
-	const RedisStore = connectRedis(session);
-	const redis = new Redis();
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "qowiueojwojfalksdjoqiwueo",
+      resave: false,
+    })
+  );
 
-	app.use(cors({
-		origin: "http://localhost:3000",
-		credentials: true
-	}))
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({ req, res, redis }),
+  });
 
-	app.use(
-		session({
-			name: COOKIE_NAME,
-			store: new RedisStore({
-				client: redis,
-				disableTouch: true
-			}),
-			cookie: {
-				maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // Equal to 10 years
-				httpOnly: true,
-				sameSite: 'lax', // CSRF
-				secure: __prod__, // Cookies only working on https
-			},
-			saveUninitialized: false,
-			secret: 'hdyrbunshtpldecgfdhuydra', // Handle with env variables later !! 
-			resave: false,
-		})
-	)
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
-	// Resolvers
-	const apolloServer = new ApolloServer({
-		schema: await buildSchema({
-			resolvers: [HelloResolver, PostResolver, UserResolver],
-			validate: false
-		}),
-		context: ({ req, res }) => ({ req, res, redis })
-	});
-
-	apolloServer.applyMiddleware({ app, cors: false });
-
-	app.listen(4000, () => {
-		console.log('🟢 Server: http://localhost:4000');
-	});
-}
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
 
 main().catch((err) => {
-	console.error(err);
+  console.error(err);
 });
