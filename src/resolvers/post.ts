99a868af57ed82e5a056b481ce2603b91317a689
rust7 <rust.91@icloud.com>
2020-11-03 19:32:04 +0100

    Graphql / URQL Pagination - Part 1

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,7 +1,8 @@
 import { isAuth } from "../middleware/isAuth";
 import { MyContext } from "../types";
-import { Resolver, Query, Arg, Mutation, InputType, Field, Ctx, UseMiddleware } from "type-graphql";
+import { Resolver, Query, Arg, Mutation, InputType, Field, Ctx, UseMiddleware, Int } from "type-graphql";
 import { Post } from "../entities/Post";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -15,8 +16,24 @@ class PostInput {
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    async posts(): Promise<Post[]> {
-        return Post.find();
+    async posts(
+        @Arg("limit", () => Int) limit: number,
+        @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+    ): Promise<Post[]> {
+        const realLimit = Math.min(50, limit);
+        const qb = getConnection()
+            .getRepository(Post)
+            .createQueryBuilder("p")
+            .orderBy('"createdAt"', 'DESC')
+            .take(realLimit)
+
+            if (cursor) {
+                qb.where('"createdAt" < :cursor', { 
+                    cursor: new Date(parseInt(cursor))
+                })
+            }
+
+            return qb.getMany();
     }
 
     // Fetch Single Post
@@ -48,12 +65,10 @@ export class PostResolver {
         
         if (!post) {
             return null
-        } 
-        
+        }
         if (typeof title !== 'undefined') {
             await Post.update({ id }, { title })
         }
-
         return post;
     }
 
